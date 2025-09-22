e70df002d8caadc25557a626e50e08b0191dad8b
Philipp Strathausen <strathausen@gmail.com>
2025-09-22 13:13:47 +0200

    fix build

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
+
+type Attribute = "class" | "data-theme" | "data-mode";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: Attribute | Attribute[];
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
